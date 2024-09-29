ba64cf850a83197c949bee84fbeaa7d4cdbc5f48
srksifat-dev <srksifat.dev@gmail.com>
2024-09-29 21:37:37 +0600

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Md. Shoibur Rahman Khan Sifat on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConvertTests/ConvertTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertTests.swift
+//  ConvertTests
+//
+//  Created by Md. Shoibur Rahman Khan Sifat on 9/29/24.
+//
+
+import Testing
+@testable import Convert
+
+struct ConvertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
