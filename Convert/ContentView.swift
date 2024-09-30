397c1f3883c3ce8221fc0e27c7857d0a26cf6744
srksifat-dev <srksifat.dev@gmail.com>
2024-09-30 12:45:25 +0600

    Challenge is completed!

--- Convert/ContentView.swift
+++ Convert/ContentView.swift
@@ -31,6 +31,7 @@ struct ContentView: View {
     
     @State private var input: Double = 0.0
     @State private var output: Double = 0.0
+    @FocusState private var isFocused: Bool
     
     func convert(from inputUnit: String, to outputUnit: String, input: Double) -> Double{
         switch (inputUnit,outputUnit){
@@ -41,7 +42,7 @@ struct ContentView: View {
         case ("Fahrenheit", "Celcius"):
             return (input - 32) * 5/9
         case ("Fahrenheit", "Kelvin"):
-            return (input - 3) * 5/9
+            return (input - 32) * 5/9 + 273.15
         case ("Kelvin", "Celcius"):
             return input - 273.15
         case ("Kelvin", "Fahrenheit"):
@@ -53,7 +54,7 @@ struct ContentView: View {
         case ("Meters", "Yards"):
             return input * 1.09361
         case ("Meters", "Miles"):
-            return input * 0.0006
+            return input * 0.000621371
         case ("Kilometers", "Meters"):
             return input * 1000
         case ("Kilometers", "Feet"):
@@ -63,25 +64,25 @@ struct ContentView: View {
         case ("Kilometers", "Miles"):
             return input * 0.621371
         case ("Feet", "Meters"):
-            return input / 3.28084
+            return input * 0.3048
         case ("Feet", "Kilometers"):
-            return input / 3280.84
+            return input * 0.0003048
         case ("Feet", "Yards"):
-            return input / 3.28084
+            return input * 0.333333
         case ("Feet", "Miles"):
-            return input / 5280.00
+            return input * 0.000189394
         case ("Yards", "Meters"):
-            return input / 1.09361
+            return input * 0.9144
         case ("Yards", "Kilometers"):
-            return input / 1093.61
+            return input * 0.0009144
         case ("Yards", "Feet"):
-            return input * 3.28084
+            return input * 3
         case ("Yards", "Miles"):
             return input * 0.000538813
         case ("Miles", "Meters"):
             return input * 1609.344
         case ("Miles", "Kilometers"):
-            return input * 1609.344 / 1.609344
+            return input * 1.60934
         case ("Miles", "Feet"):
             return input * 5280.00
         case ("Miles", "Yards"):
@@ -113,43 +114,43 @@ struct ContentView: View {
         case ("Milliliters","Liters"):
             return input / 1000
         case ("Milliliters","Cups"):
-            return input / 236.58823659
+            return input * 0.0042267
         case ("Milliliters","Pints"):
-            return input / 473.1764731
+            return input * 0.0021133
         case ("Milliliters","Gallons"):
-            return input / 3785.4117847
+            return input * 0.000264172
         case ("Liters","Milliliters"):
             return input * 1000
         case ("Liters","Cups"):
-            return input * 4.54609177
+            return input * 4.22675
         case ("Liters","Pints"):
-            return input * 1.3333333333333333
+            return input * 2.11338
         case ("Liters","Gallons"):
             return input * 0.26417205393700787
         case ("Cups","Milliliters"):
             return input * 236.58823659
         case ("Cups","Liters"):
-            return input / 4.54609177
+            return input * 0.236588
         case ("Cups","Pints"):
-            return input / 1.7320508075629696
+            return input * 0.5
         case ("Cups","Gallons"):
-            return input / 7.4635294118
+            return input * 0.0625
         case ("Pints","Milliliters"):
             return input * 473.1764731
         case ("Pints","Liters"):
-            return input * 1.3333333333333333
+            return input * 0.473176
         case ("Pints","Cups"):
-            return input * 2.3658823659
+            return input * 2
         case ("Pints","Gallons"):
-            return input * 0.86602540378148142
+            return input * 0.125
         case ("Gallons","Milliliters"):
             return input * 3785.4117847
         case ("Gallons","Liters"):
-            return input * 0.26417205393700787
+            return input * 3.78541
         case ("Gallons","Cups"):
-            return input * 7.4635294118
+            return input * 16
         case ("Gallons","Pints"):
-            return input * 2.6417205393700787
+            return input * 8
         default:
             return input
         }
@@ -168,34 +169,46 @@ struct ContentView: View {
                             if let units = conversion[selectedConversion] {
                                 selectedInputUnit = units.first ?? ""
                                 selectedOutputUnit = units[1]
+                                input = 0
                             }
                         }
                         .padding(.horizontal,16)
                         Form {
                             Section("Convert"){
-                                HStack{
-                                    TextField("Enter Value", value: $input,format: .number)
-                                        .keyboardType(.decimalPad)
-                                    Picker("", selection: $selectedInputUnit) {
-                                        ForEach(conversion[selectedConversion] ?? [], id: \.self) {
-                                            Text($0)
+                                GeometryReader{geometry in
+                                    HStack{
+                                        TextField("Enter Value", value: $input,format: .number)
+                                            .keyboardType(.decimalPad)
+                                            .focused($isFocused)
+                                        Picker("", selection: $selectedInputUnit) {
+                                            ForEach(conversion[selectedConversion] ?? [], id: \.self) {
+                                                Text($0)
+                                            }
                                         }
+                                        .frame(width: geometry.size.width * 0.4)
+                                        
                                     }
-                                    
                                 }
+                                
                                 
                             }
                             
                             
                             Section("To"){
-                                HStack{
-                                    Text("\(convert(from: selectedInputUnit, to: selectedOutputUnit, input: input),specifier: "%.2f")")
-                                    Picker("", selection: $selectedOutputUnit) {
-                                        ForEach(conversion[selectedConversion] ?? [], id: \.self) {
-                                            Text($0)
+                                GeometryReader{geometry in
+                                    HStack{
+                                        Text("\(convert(from: selectedInputUnit, to: selectedOutputUnit, input: input))")
+                                            
+                                        Spacer()
+                                        Picker("", selection: $selectedOutputUnit) {
+                                            ForEach(conversion[selectedConversion] ?? [], id: \.self) {
+                                                Text($0)
+                                            }
                                         }
+                                        .frame(width: geometry.size.width * 0.4)
                                     }
                                 }
+                                
                             }
                            
                         }
@@ -206,6 +219,13 @@ struct ContentView: View {
                     
                     .navigationTitle("Convert")
                     .navigationBarTitleDisplayMode(.inline)
+                    .toolbar{
+                        if isFocused{
+                            Button("Done"){
+                                isFocused = false
+                            }
+                        }
+                    }
                     
                 }
             }
